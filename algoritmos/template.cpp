d374cc01594db35470f40b24f98d2ca8ffdc7b57
Eloy Maciel <105126138+El0y-C0SM0@users.noreply.github.com>
2025-08-21 23:05:14 -0300

    Refactor macros and type definitions in template.cpp

--- algoritmos/template.cpp
+++ algoritmos/template.cpp
@@ -1,18 +1,19 @@
 #include <bits/stdc++.h>
 
-#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(NULL);
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
 #define endl '\n'
 #define dbg(a) (cout << #a << " = " << a << '\n')
-#define loop(i,a,m) for(int i = a; i < m; ++i)
+#define loop(i,a,m) for(int i = a; i < m; ++i) 
 #define pb push_back
-#define f first
-#define s second
+#define ff first
+#define ss second
 
 using namespace std;
 
 typedef uint32_t uint;
 typedef long long ll;
 typedef long double ld;
+typedef unsigned long long ull;
 typedef vector<int> vtr;
 typedef pair<int,int> par;
 
